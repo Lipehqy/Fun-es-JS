6d7cf9c80c4c134160b692f2bc71e226def79bdd
Felipe <chagasfelipe2008@gmail.com>
2026-02-24 16:28:26 -0300

    atualizar

--- exercicios.js
+++ exercicios.js
@@ -81,3 +81,86 @@ function somaTotal(){
     return soma;
 }
 console.log(somaTotal())
+
+
+// Ex 12
+function contarParesAte10() {
+let contador = 0;
+for (let i = 1; i <= 10; i++) {
+if (i % 2 === 0) {
+contador++;
+}
+}
+return contador;
+}
+console.log(contarParesAte10());
+
+
+
+// Ex 13
+function tabuada(numero) {
+for (let i = 1; i <= 10; i++) {
+console.log(numero + " x " + i + " = " + (numero * i));
+}
+}
+tabuada(5);
+
+
+//  Ex 14
+function contagemRegressiva() {
+for (let i = 10; i >= 1; i--) {
+console.log(i);
+}
+}
+contagemRegressiva();
+
+
+// Ex 15
+function encontrar27() {
+for (let i = 1; i <= 50; i++) {
+if (i === 27) {
+return "Número 27 encontrado!";
+}
+}
+}
+console.log(encontrar27());
+
+
+// Ex 16
+function somarPares(limite) {
+let soma = 0;
+for (let i = 1; i <= limite; i++) {
+if (i % 2 === 0) {
+soma += i;
+}
+}
+return soma;
+}
+console.log(somarPares(10));
+
+
+// Ex 17
+function contarImpares() {
+let contador = 0;
+for (let i = 1; i <= 10; i++) {
+if (i % 2 !== 0) {
+contador++;
+}
+}
+return contador;
+}
+console.log(contarImpares());
+
+
+// Ex 18
+function media(a, b) {
+return (a + b) / 2;
+}
+console.log(media(8, 6));
+
+
+// Ex 19
+function quadrado(numero) {
+return numero * numero;
+}
+console.log(quadrado(4));
